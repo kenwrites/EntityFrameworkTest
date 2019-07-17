68bef939bf66b24dc74412543e314c914124609c
kenwrites <28812088+kenwrites@users.noreply.github.com>
2019-07-17 13:56:55 -0400

    Initial commit

---
+++ EntityFrameworkTest/Model.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EntityFrameworkTest
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+            sb.DataSource = "(local)\\SQLExpress";
+            sb.UserID = "sa";
+            sb.Password = "LovelyB123!";
+            sb.InitialCatalog = "Blogging";
+            sb.IntegratedSecurity = true;
+            optionsBuilder.UseSqlServer(sb.ConnectionString);
+        }
+    }
+
+  
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
+    }
+}
---
+++ EntityFrameworkTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace EntityFrameworkTest
+{
+    class Program
+    {
+        static void Main()
+        {
+           using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine($"{count} records saved to database. \r\n");
+
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine($" - {blog.Url}");
+                }
+            }
+        }
+    }
+}
